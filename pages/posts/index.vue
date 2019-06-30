9316a9da71cb807c708c3c318d1e7abac7fde756
Dmitry <tdmitriiv@gmail.com>
2019-06-30 16:49:33 +0400

    Base blog page routing

---
+++ pages/posts/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="posts-page">
+
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "Posts"
+  }
+</script>
+
+<style scoped>
+  .posts-page {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
